827ac129bf477ca92f05a831d7b9bb1fc68e9bf5
Yuvraj Patel <yuvipatel003@gmail.com>
2021-01-25 21:02:52 -0500

    remove networkcall to check api. And Added recylerview.

--- Carfax/app/src/main/java/com/appsdeviser/carfax/ui/listing/MainActivity.kt
+++ Carfax/app/src/main/java/com/appsdeviser/carfax/ui/listing/MainActivity.kt
@@ -3,10 +3,15 @@ package com.appsdeviser.carfax.ui.listing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.appsdeviser.carfax.R
 import com.appsdeviser.carfax.data.api.ApiInterface
 import com.appsdeviser.carfax.data.xxx.CarFaxData
+import com.appsdeviser.carfax.databinding.ActivityMainBinding
 import com.appsdeviser.carfax.ui.di.Injector
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,42 +21,54 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    private val mTAG = MainActivity::class.java.simpleName
     @Inject
     lateinit var mFactory: ListingViewModelFactory
 
-    private lateinit var mNewsViewModel: ListingViewModel
+    private lateinit var mViewModel: ListingViewModel
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mAdapter: ListingRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // getTeamsList()
-
         (application as Injector).createListingSubComponent()
             .inject(this)
 
-        mNewsViewModel = ViewModelProvider(this, mFactory).get(ListingViewModel::class.java)
+        mViewModel = ViewModelProvider(this, mFactory).get(ListingViewModel::class.java)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        mNewsViewModel.getCarFaxData().observe(this,{
+        mViewModel.getCarFaxData().observe(this, Observer {
             Log.d("Data : ", it.toString())
+        })
+
+
+        initRecyclerView()
+    }
+
+    /**
+     * Initialize RecyclerView
+     */
+    private fun initRecyclerView() {
+        mBinding.recyclerViewListing.layoutManager = LinearLayoutManager(this)
+        mAdapter = ListingRecyclerViewAdapter()
+        mBinding.recyclerViewListing.adapter = mAdapter
+        displayAllListing()
+    }
+
+
+    private fun displayAllListing() {
+        mViewModel.getCarFaxData().observe(this, Observer{
+
+            val list = it?.listings
+            Log.d(mTAG, "Item Size: ${list?.size}")
+            if(list != null) {
+                mAdapter.setList(list)
+                mAdapter.notifyDataSetChanged()
+            }
+
         })
     }
 
-
-
-    /**
-     * get list of teams
-     * make a network call using ApiInterface(retrofit) through Co-routine
-     */
-    private fun getTeamsList() {
-        Log.d("TAG", "getTeamsList()")
-        val apiInterface = ApiInterface.create().getCarFaxData()
-
-        // Network call using Co-routine
-        CoroutineScope(Dispatchers.IO).launch {
-            val data: Response<CarFaxData> = apiInterface.execute()
-            val listOfTeams = data.body()
-            Log.d("Data : ", listOfTeams.toString())
-        }
-    }
 }
